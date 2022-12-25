8a2b724966f66d782df98d3ce552cca363246480
denispetko <den.petko@rambler.ru>
2022-12-25 22:09:55 +0300

    initial

---
+++ src/Employee.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+public class Employee {
+    private String fullName;
+    private int department;
+    private int salary;
+    private final int id;
+    static int count = 0;
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static void printEmployee (Employee[] employees) {
+        System.out.println(Arrays.toString(employees));
+    }
+    public static void findSumSalary(Employee[] employees) {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary(); //
+        }
+        System.out.println("" + sum);
+    }
+    public static void indexOn (Employee[] employees, double index) {
+        for (Employee employee : employees) {
+            employee.setSalary((int) (employee.getSalary()*index));
+        }
+        System.out.println(Arrays.toString(employees));
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = count;
+        count++;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Иванов Иван Иванович", 1, 15000 );
+        employees[1] = new Employee("Борисов Борис Борисович", 1, 16000 );
+        employees[2] = new Employee("Денисов Денис Денисович", 2, 17000 );
+        employees[3] = new Employee("Адамович Татьяна Владимировна", 2, 18000 );
+        employees[4] = new Employee("Алешин Сергей Сергеевич", 3, 19000 );
+        employees[5] = new Employee("Андреева Регина Эгартовна", 3, 12000 );
+        employees[6] = new Employee("Астафьева Ирина Николаевна", 4, 11000 );
+        employees[7] = new Employee("Базыкин Георгий Александрович", 4, 150000 );
+        employees[8] = new Employee("Бастаков Владимир Антонинович", 5, 155000 );
+        employees[9] = new Employee("Белецкая Наталия Владимировна", 5, 156000 );
+
+        Employee.printEmployee(employees);
+        Employee.findSumSalary(employees);
+        Employee.indexOn(employees, 1.083);
     }
+
 }
